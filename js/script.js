acb6084d5afc0e37cad1fe9d35db1cd4c0a27919
Zachary-Rossman <zrossman@yahoo.com>
2023-05-24 03:46:36 -0500

    added text to question area

--- js/script.js
+++ js/script.js
@@ -4,10 +4,16 @@ let questionDiv = document.querySelector(`#question`);
 let answerDiv = document.querySelector(`#answerChoices`);
 
 // Initial State
-function init(){};
+function init(){
+    console.log('This is my initial state. Welcome!');
+};
 
 // Functions
+function startQuiz(event){
+    console.log(event);
+    console.log(`This function will begin the game`);
+};
 
 // Event Listeners and Init call
 init();
-playBtn.addEventListener("click", init);
+playBtn.addEventListener("click", startQuiz);
