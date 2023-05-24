d8df91f7816b836f66bdf8f7f326ca3dfac833ea
Zachary-Rossman <zrossman@yahoo.com>
2023-05-24 03:39:23 -0500

    added very basic code into script.js file; added areas needed for quiz into HTML file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // Global Variables
+let playBtnDiv = document.querySelector(`#playBtn`);
+let questionDiv = document.querySelector(`#question`);
+let answerDiv = document.querySelector(`#answerChoices`);
 
 // Initial State
 function init(){};
@@ -7,3 +10,4 @@ function init(){};
 
 // Event Listeners and Init call
 init();
+playBtn.addEventListener("click", init);
