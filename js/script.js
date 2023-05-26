6124316e3d3f99543b55c6679805a4965dc48afa
Zachary-Rossman <zrossman@yahoo.com>
2023-05-26 00:45:20 -0500

    added second question; user is now allowed to select answer; score still not incrementing properly

--- js/script.js
+++ js/script.js
@@ -27,6 +27,9 @@ let score = [`${correct - incorrect}/50`];
 
 // Initial State
 function init(){
+    // Hide answer options in intial state; Only showing when questions are being asked
+    answerDiv.style.display = 'none';
+    // Directions for user
     questionDiv.innerHTML = `Click the button below to start the quiz.`;
 
     // When button is clicked, quiz begins
@@ -37,37 +40,197 @@ function init(){
 
 // Function for first state; Will be repeated 50 times in alphabetical order
 function q1(){
+    
+    // Displays question for user to see
     let q1 = `What is the capitol of ${states[0]}`;
     questionDiv.innerHTML = q1;
+    
+    // Answer Choices
     let a1 = ['Birmingham', 'Biloxi', 'Talladega', `${capitols[0]}`];
-    playBtnDiv.innerHTML = "Next State";
-    let r1 = [""];
 
+    // Displays answers for user to select
+    answerDiv.style.display = 'flex';
+    answerDiv.style = 'flex-direction: column';
+
+    // Change text in button for user directions
+    playBtnDiv.style.display = 'none';
+
+    // Bind options to the a1 array
     let answerOption1 = a1[0];
     let answerOption2 = a1[1];
     let answerOption3 = a1[2];
     let answerOption4 = a1[3];
 
+    // Displays option text in the HTML
     aTxt.innerHTML = `${answerOption1}`;
     bTxt.innerHTML = `${answerOption2}`;
     cTxt.innerHTML = `${answerOption3}`;
     dTxt.innerHTML = `${answerOption4}`;
 
-    a.addEventListener("input", checkA1);
-    b.addEventListener("iput", checkA1);
-    c.addEventListener("input", checkA1);
-    d.addEventListener("input", checkA1);
+    // Event listeners for each option
+    a.addEventListener("input", function checkA(){
+        if (a.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
 
+            // Hides answers to reset buttons
+            answerDiv.style.display = 'none';
+
+            // Calls next function
+            q2();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    b.addEventListener("input", function checkB(){
+        if (b.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
+
+            // Hides answers to reset buttons
+            answerDiv.style.display = 'none';
+
+            // Calls next function
+            q2();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    c.addEventListener("input", function checkC(){
+        if (c.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
+
+            // Hides answers to reset buttons
+            answerDiv.style.display = 'none';
+
+            // Calls next function
+            q2();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    d.addEventListener("input", function checkD(){
+        if (d.input = true){
+            // Increments 1 to correct and stores to local storage; raises score by 1
+            correct ++;
+
+            // Hides answers to reset buttons
+            answerDiv.style.display = 'none';
+
+            // Calls next function
+            q2();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    // End of this question's function
 };
 
+function q2(){
+    
+    // Displays question for user to see
+    let q2 = `What is the capitol of ${states[1]}`;
+    questionDiv.innerHTML = q2;
+    
+    // Answer Choices
+    let a2 = ['Anchorage', `${capitols[1]}`, 'Yukon', 'Fairbanks'];
 
-function checkA1(){
-    if (q1 === q1.answerOption4) {
-        console.log('correct');
-    } else {
-        console.log('Incorrect');
-    }
-}
+    // Displays answers for user to select
+    answerDiv.style.display = 'flex';
+    answerDiv.style = 'flex-direction: column';
+
+    // Change text in button for user directions
+    playBtnDiv.style.display = 'none';
+
+    // Bind options to the a1 array
+    let answerOption1 = a2[0];
+    let answerOption2 = a2[1];
+    let answerOption3 = a2[2];
+    let answerOption4 = a2[3];
+
+    // Displays option text in the HTML
+    aTxt.innerHTML = `${answerOption1}`;
+    bTxt.innerHTML = `${answerOption2}`;
+    cTxt.innerHTML = `${answerOption3}`;
+    dTxt.innerHTML = `${answerOption4}`;
+
+    // Event listeners for each option
+    a.addEventListener("input", function checkA(){
+        if (a.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
+
+            // Calls next function
+            showResults();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    b.addEventListener("input", function checkB(){
+        if (b.input = true){
+            // Increments 1 to correct and stores to local storage; raises score by 1
+            correct ++;
+
+            // Calls next function
+            showResults();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    c.addEventListener("input", function checkC(){
+        if (c.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
+
+            // Calls next function
+            showResults();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    d.addEventListener("input", function checkD(){
+        if (d.input = true){
+            // Increments 1 to incorrect and stores to local storage; lowers score by 1
+            incorrect ++;
+
+            // Calls next function
+            showResults();
+
+            // Base to be used later for reset of user selection
+            // playBtnDiv.style.display = 'flex';
+            // playBtnDiv.innerHTML = 'Next State';
+            // playBtnDiv.addEventListener("click", q2);
+        }
+    });
+    // End of this question's function
+};
+
+// Shows results at end of quiz from local storage
+function showResults(){
+    resultsDiv.innerHTML = score;
+};
 
 // Event Listeners and Init call
 init();
